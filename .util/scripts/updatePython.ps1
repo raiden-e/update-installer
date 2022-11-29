8d178591e146b1b35ff2cdb9a2cf9da840cef35f
Raiden Erdmann <raiden.erdmann@gmail.com>
2022-11-29 17:46:36 +0100

    fix python versioning

--- .util/scripts/updatePython.ps1
+++ .util/scripts/updatePython.ps1
@@ -1,8 +1,15 @@
 function main {
     param()
-    $mainlink = "https://www.python.org/ftp/python/"
-    $page = Invoke-WebRequest $mainlink -UseBasicParsing
-    $latestVersion = ($page.Links.href | ? { $_ -match "(?:\d+\.){2}\d+" } | Select-Object -Last 1).trim("/\")
-    return "Python", "python-$latestVersion.exe", "python-*.exe", "$mainLink$latestVersion/python-$latestVersion.exe"
+    function Get-Version ($link) {
+        return $page.Links.href | ? { $_ -match "(?:\d+\.){2}\d+" } | % { [System.Version]($_.Trim("/\")) } | Sort-Object
+    }
+    $mainlink = "https://www.python.org/ftp/python"
+    $page = Invoke-WebRequest -UseBasicParsing $mainlink
+    $latestVersion = "$((Get-Version)[-1])"
+    $page2 = Invoke-WebRequest -UseBasicParsing "$mainlink/$latestVersion"
+    if ("python-$latestVersion.exe" -notin $page2.Links.href) {
+        $latestVersion = "$((Get-Version)[-2])"
+    }
+    return "Python", "python-$latestVersion.exe", "python-*.exe", "$mainLink/$latestVersion/python-$latestVersion.exe"
 }
 return main
