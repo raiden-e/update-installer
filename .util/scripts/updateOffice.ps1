b3740fc3a11d076beb6234bb8c896d41cf99fdec
Raiden Erdmann <raiden.erdmann@gmail.com>
2023-02-05 14:30:28 +0100

    Add Office installer

---
+++ .util/scripts/updateOffice.ps1
@@ -0,0 +1,8 @@
+function main {
+    param()
+    $page = Invoke-WebRequest -UseBasicParsing "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
+    $link = ($page.links | ? href -Match '/officedeploymenttool_\d{5}-\d{5}\.exe$').href | Select-Object -First 1
+    $filename = $link.Substring($link.LastIndexOf("/") + 1)
+    return "Process Explorer", "$filename", "ProcessExplorer*.zip", "$link"
+}
+return main
