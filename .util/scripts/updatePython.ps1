503dfbc88a33813b8bfa0f14107ed8caa846db3b
Raiden Erdmann <raiden.erdmann@gmail.com>
2023-02-16 00:07:57 +0100

    fix: Python 64bit

--- .util/scripts/updatePython.ps1
+++ .util/scripts/updatePython.ps1
@@ -7,9 +7,9 @@ function main {
     $page = Invoke-WebRequest -UseBasicParsing $mainlink
     $latestVersion = "$((Get-Version)[-1])"
     $page2 = Invoke-WebRequest -UseBasicParsing "$mainlink/$latestVersion"
-    if ("python-$latestVersion.exe" -notin $page2.Links.href) {
+    if ("python-$latestVersion-amd64.exe" -notin $page2.Links.href) {
         $latestVersion = "$((Get-Version)[-2])"
     }
-    return "Python", "python-$latestVersion.exe", "python-*.exe", "$mainLink/$latestVersion/python-$latestVersion.exe"
+    return "Python", "python-$latestVersion-amd64.exe", "python-*amd64.exe", "$mainLink/$latestVersion/python-$latestVersion-amd64.exe"
 }
 return main
