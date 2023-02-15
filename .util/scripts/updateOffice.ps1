3b94dc35217961e7d5066d43059f3f9614ac1a96
Raiden Erdmann <raiden.erdmann@gmail.com>
2023-02-16 00:09:03 +0100

    fix: Office updater

--- .util/scripts/updateOffice.ps1
+++ .util/scripts/updateOffice.ps1
@@ -3,6 +3,6 @@ function main {
     $page = Invoke-WebRequest -UseBasicParsing "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
     $link = ($page.links | ? href -Match '/officedeploymenttool_\d{5}-\d{5}\.exe$').href | Select-Object -First 1
     $filename = $link.Substring($link.LastIndexOf("/") + 1)
-    return "Process Explorer", "$filename", "ProcessExplorer*.zip", "$link"
+    return "Office Deployment", "$filename", "officedeploymenttool*.exe", "$link"
 }
 return main
