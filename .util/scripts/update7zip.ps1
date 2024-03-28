f3a7aec8529c877677363f302e46630f856372ef
Raiden Erdmann <raiden.erdmann@gmail.com>
2024-03-28 23:08:35 +0100

    switch to 7z exe

--- .util/scripts/update7zip.ps1
+++ .util/scripts/update7zip.ps1
@@ -1,9 +1,9 @@
 function main {
     param()
     $page = Invoke-WebRequest -UseBasicParsing "https://www.7-zip.de/download.html"
-    $latestLink = ($page.Links | ? { $_.href -like "https://7-zip.org/a/7z*x64.msi" }).href | Select-Object -First 1
+    $latestLink = ($page.Links | ? { $_.href -like "https://7-zip.org/a/7z*x64.exe" }).href | Select-Object -First 1
     $latest = $latestLink.Substring($latestLink.LastIndexOf("/") + 1)
-    return "7-Zip", $latest, "7z*x64.msi", $latestLink
+    return "7-Zip", $latest, "7z*x64.exe", $latestLink
 }
 
 return main
