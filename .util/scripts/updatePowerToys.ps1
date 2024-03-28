9b608cbec0f6f4e7488c6b17051b23eb10f48fd5
Raiden Erdmann <raiden.erdmann@gmail.com>
2024-03-28 23:08:47 +0100

    fix MS PowerToys updater

--- .util/scripts/updatePowerToys.ps1
+++ .util/scripts/updatePowerToys.ps1
@@ -4,6 +4,6 @@ function main {
     $asset = $rest.assets | ? { $_.Name -like "*x64.exe" -and $_.Name -notlike "*User*" }
     $filename = $asset.Name
     $link = $asset.browser_download_url
-    return "PowerToys", $filename, "PowerToys*.msi", $link
+    return "PowerToys", $filename, "PowerToys*.exe", $link
 }
 return main
